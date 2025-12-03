fbf702c9e4ebf3ccad9ce3ea690c5a84909863e4
clinaresm <camilo.linaresm@autonoma.edu.co>
2025-12-03 10:09:24 -0500

    Conexiones a SQL en Azure Productivas

--- database.py
+++ database.py
@@ -6,13 +6,23 @@ load_dotenv()
 
 def get_connection():
     try:
+        # conn = pyodbc.connect(
+        #     f"DRIVER={{{os.getenv('DRIVER')}}};"
+        #     f"SERVER={os.getenv('DB_SERVER')};"
+        #     f"DATABASE={os.getenv('DB_NAME')};"
+        #     "Encrypt=yes;"
+        #     "TrustServerCertificate=yes;"
+        #     "Trusted_Connection=yes;"
+        # )
         conn = pyodbc.connect(
             f"DRIVER={{{os.getenv('DRIVER')}}};"
-            f"SERVER={os.getenv('DB_SERVER')};"
+            f"SERVER={os.getenv('DB_SERVER')},1433;"
             f"DATABASE={os.getenv('DB_NAME')};"
+            f"UID={os.getenv('DB_USER')};"
+            f"PWD={os.getenv('DB_PASSWORD')};"
             "Encrypt=yes;"
-            "TrustServerCertificate=yes;"
-            "Trusted_Connection=yes;"
+            "TrustServerCertificate=no;"
+            "Connection Timeout=30;"
         )
         return conn
     except Exception as e:
